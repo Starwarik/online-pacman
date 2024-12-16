db193d09f0bb0ee49d1511af4066071b96f49237
starwarik@bk.ru <starwarik@bk.ru>
2024-12-16 11:49:40 +0000

    Created canvas; Edited settings; Deleted Form1

--- client_code/canvas/__init__.py
+++ client_code/canvas/__init__.py
@@ -1,10 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import canvasTemplate
 from anvil import *
 
 
-class Form1(Form1Template):
+class canvas(canvasTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.cpan
+    
